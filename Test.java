649314c0964f5d64047f492920d2926d5f54f5e2
akashcoderr <42908574+akashcoderr@users.noreply.github.com>
2018-10-14 08:43:40 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author The Robust Coder
+ */
+public class Test {
+    public static void main(String[] arr){
+        int count=0;
+        Pattern p=Pattern.compile("[a-zA-Z0-9][a-zA-Z0-9$_.]*[.]java");
+        File f=new File("D:\\java\\java");
+        String[] s=f.list();
+        for(String s1:s){
+            Matcher m=p.matcher(s1);
+            if(m.find()&&m.group().equals(s1)){
+                count++;
+                System.out.println(s1);
+            }
+        }
+        System.out.println("The Total Number Is :"+count);
+    }
+}
